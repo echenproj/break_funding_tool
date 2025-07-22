71ae5cb5f32e407195b6f746010d70c579ccc02d
Elaine Chen <56491773+echenproj@users.noreply.github.com>
2025-07-22 01:51:10 -0400

    Modify for render

--- app.py
+++ app.py
@@ -299,5 +299,6 @@ def download_ppt():
     )
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
